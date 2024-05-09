a6af3d0f22da0aa397c2f6347996798c2deb79aa
ianArlen <ian.street.fut@gmail.com>
2024-05-09 02:03:04 -0600

    Random title in images

--- src/app/loader/loader.page.ts
+++ src/app/loader/loader.page.ts
@@ -16,25 +16,43 @@ export class LoaderPage implements OnInit, OnDestroy {
     'https://images.pexels.com/photos/1619317/pexels-photo-1619317.jpeg',
     'https://content.nationalgeographic.com.es/medio/2018/02/27/playa-de-isuntza-lekeitio__1280x720.jpg'
   ];
+  titles = ['Forest Landscape', 'Misty Mountains', 'Sunny Beach'];
+  usedTitles = new Set<string>(); // Set to track used titles
   currentIndex = 0;
   currentImage = this.images[0];
-  private intervalId: any;
+  currentTitle = this.titles[0];
+  private timeoutId: any;
 
   constructor() { }
 
   ngOnInit() {
-    this.intervalId = setInterval(() => {
-      this.currentIndex++;
-      if (this.currentIndex >= this.images.length) {
-        this.currentIndex = 0;
-      }
-      this.currentImage = this.images[this.currentIndex];
-    }, 4000); 
+    this.updateImage();
   }
 
   ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
     }
+  }
+
+  private updateImage(): void {
+    this.currentImage = this.images[this.currentIndex];
+    this.currentTitle = this.getRandomTitle();
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+
+    this.timeoutId = setTimeout(() => {
+      this.updateImage();
+    }, 4000);
+  }
+
+  private getRandomTitle(): string {
+    let possibleTitles = this.titles.filter(title => !this.usedTitles.has(title));
+    if (possibleTitles.length === 0) {
+      this.usedTitles.clear();
+      possibleTitles = this.titles;
+    }
+    const randomTitle = possibleTitles[Math.floor(Math.random() * possibleTitles.length)];
+    this.usedTitles.add(randomTitle);
+    return randomTitle;
   }
 }
